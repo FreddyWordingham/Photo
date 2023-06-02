f7e8271645592ead77bc8111c369725646e02f2b
FreddyWordingham <freddy@digilab.co.uk>
2023-06-02 20:43:08 +0100

    Added Parameters serialisation

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,19 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Parameters {
+    resolution: (u32, u32),
+}
+
 fn main() {
     println!("Hello, world!");
+
+    let json_str = r#"
+        {
+            "resolution": [1920, 1080]
+        }
+    "#;
+
+    let p: Parameters = serde_json::from_str(json_str).unwrap();
+    println!("resolution: {}x{}", p.resolution.0, p.resolution.1);
 }
