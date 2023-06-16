a04f93f21352f2add7d0914f1c15ec2f0921d978
FreddyWordingham <freddy@digilab.co.uk>
2023-06-16 17:56:47 +0100

    Changed logger level to info for web build

--- src/run.rs
+++ src/run.rs
@@ -27,7 +27,7 @@ pub fn init_logger() {
     cfg_if::cfg_if! {
         if #[cfg(target_arch = "wasm32")] {
             std::panic::set_hook(Box::new(console_error_panic_hook::hook));
-            console_log::init_with_level(log::Level::Warn).expect("Couldn't initialize logger");
+            console_log::init_with_level(log::Level::Info).expect("Couldn't initialize logger");
             log::info!("WASM logger initialized.");
         } else {
             env_logger::init();
