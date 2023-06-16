4f4c066336f7f176e8fdc9a0719e3b3bba7787e4
FreddyWordingham <freddy@digilab.co.uk>
2023-06-16 21:31:21 +0100

    Implemented window.resize method

--- src/state.rs
+++ src/state.rs
@@ -77,7 +77,14 @@ impl State {
     }
 
     fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
-        todo!()
+        if new_size.width <= 0 || new_size.height <= 0 {
+            return;
+        }
+
+        self.size = new_size;
+        self.config.width = new_size.width;
+        self.config.height = new_size.height;
+        self.surface.configure(&self.device, &self.config);
     }
 
     fn input(&mut self, event: &WindowEvent) -> bool {
