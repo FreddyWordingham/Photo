28f0ce91889300ec8048776a24a749899e45f252
FreddyWordingham <freddy@digilab.co.uk>
2023-10-05 20:17:51 +0100

    Abstract u32 linear GPU processor

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,114 +1,218 @@
-use pixels::{Error, Pixels, SurfaceTexture};
-use rayon::prelude::*;
-use std::time::{Duration, Instant};
-use winit::dpi::LogicalSize;
-use winit::event::{Event, WindowEvent};
-use winit::event_loop::EventLoop;
-use winit::window::WindowBuilder;
+use pollster::FutureExt;
+use std::{borrow::Cow, str::FromStr};
 
-use photo::canvas::Canvas;
-use photo::keypress::handle_keypress;
-use photo::state::State;
+fn main() {
+    let mut numbers = read_input_numbers();
+    println!(" Input: {:?}", numbers);
 
-const WIDTH: u32 = 400;
-const HEIGHT: u32 = 300;
-const FRAMES_PER_SECOND: f64 = 10000.0;
-const SCALE: u32 = 3;
+    let hardware = (Hardware::new()).block_on();
+    let shaders = Shaders::new(
+        &hardware,
+        &numbers,
+        vec![include_str!("shader.wgsl"), include_str!("double.wgsl")],
+    );
 
-fn main() -> Result<(), Error> {
-    let event_loop = EventLoop::new();
-    let window = WindowBuilder::new()
-        .with_title("Pixels Example")
-        .with_inner_size(LogicalSize::new(
-            (WIDTH * SCALE) as f32,
-            (HEIGHT * SCALE) as f32,
-        ))
-        .build(&event_loop)
-        .unwrap();
+    for _ in 0..20 {
+        numbers = shaders.run(&mut numbers, 0).block_on();
+        println!("Output: {:?}", numbers);
+    }
 
-    let mut pixels = {
-        let window_size = window.inner_size();
-        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
-        Pixels::new(WIDTH, HEIGHT, surface_texture)?
-    };
-
-    let mut canvas = Canvas::new(WIDTH, HEIGHT);
-    let mut state = State { save_image: false };
-
-    let mut last_frame_time = Instant::now();
-    let frame_duration = Duration::from_millis((1000.0 / FRAMES_PER_SECOND) as u64);
-
-    let mut x = 0;
-    let y = HEIGHT / 2;
-    let r = 15;
-    event_loop.run(move |event, _, control_flow| {
-        if x > WIDTH {
-            x = 0;
-        }
-        x += 1;
-
-        match event {
-            Event::WindowEvent { event, .. } => match event {
-                WindowEvent::KeyboardInput { input, .. } => {
-                    if let Some(keycode) = input.virtual_keycode {
-                        handle_keypress(keycode, input.state, control_flow, &mut state);
-                    }
-                }
-                WindowEvent::Resized(new_size) => pixels
-                    .resize_surface(new_size.width, new_size.height)
-                    .expect("Failed to resize surface"),
-                _ => (),
-            },
-            Event::MainEventsCleared => {
-                let now = Instant::now();
-                let elapsed = now - last_frame_time;
-                if elapsed >= frame_duration {
-                    last_frame_time = now;
-                    // Your update logic here
-                } else {
-                    std::thread::sleep(frame_duration - elapsed);
-                }
-            }
-            _ => (),
-        }
-
-        // Handle controls
-        {
-            if state.save_image {
-                canvas.save_to_png("image.png");
-                state.save_image = false;
-            }
-        }
-
-        random_white_pixels(&mut canvas);
-        canvas.render(pixels.frame_mut());
-        pixels.render().expect("Failed to render");
-
-        winit::event_loop::ControlFlow::WaitUntil(Instant::now() + Duration::from_millis(10000));
-    });
+    numbers = shaders.run(&mut numbers, 1).block_on();
+    println!("Output: {:?}", numbers);
 }
 
-use rand::Rng;
-use std::sync::{Arc, Mutex};
-fn random_white_pixels(canvas: &mut Canvas) {
-    let n_threads = 4;
-    let n_pixels_per_thread = 1;
+fn read_input_numbers() -> Vec<u32> {
+    if std::env::args().len() <= 1 {
+        let default = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        println!("No numbers were provided, defaulting to {default:?}");
+        return default;
+    }
 
-    let canvases = Arc::new(Mutex::new(Vec::new()));
+    std::env::args()
+        .skip(1)
+        .map(|s| u32::from_str(&s).expect("Input must be a list of positive integers"))
+        .collect()
+}
 
-    (0..n_threads).into_par_iter().for_each(|_| {
-        let mut rng = rand::thread_rng();
-        let mut canvas_copy = canvas.clone();
-        for _ in 0..n_pixels_per_thread {
-            let x = rng.gen_range(0..canvas.width());
-            let y = rng.gen_range(0..canvas.height());
-            canvas_copy.draw_pixel(x, y, [0xff, 0xff, 0xff, 0xff]); // Make sure draw_pixel is thread-safe
+use wgpu;
+use wgpu::util::DeviceExt;
+
+struct Hardware {
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+}
+
+impl Hardware {
+    async fn new() -> Self {
+        // Instantiates instance of WebGPU
+        let instance = wgpu::Instance::default();
+
+        // `request_adapter` instantiates the general connection to the GPU
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions::default())
+            .await
+            .expect("No suitable GPU adapters found on the system!");
+
+        // `request_device` instantiates the feature specific connection to the GPU.
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    label: None,
+                    features: wgpu::Features::empty(),
+                    limits: wgpu::Limits::downlevel_defaults(),
+                },
+                None,
+            )
+            .await
+            .unwrap();
+
+        Self { device, queue }
+    }
+}
+
+struct Shaders<'a> {
+    hardware: &'a Hardware,
+    buffer_size: usize,
+    storage_buffer: wgpu::Buffer,
+    staging_buffer: wgpu::Buffer,
+    compute_pipelines: Vec<wgpu::ComputePipeline>,
+    bind_groups: Vec<wgpu::BindGroup>,
+}
+
+impl<'a> Shaders<'a> {
+    fn new(hardware: &'a Hardware, numbers: &[u32], shader_codes: Vec<&str>) -> Self {
+        let buffer_size = std::mem::size_of_val(numbers);
+
+        // Instantiates buffer without data.
+        let staging_buffer = hardware.device.create_buffer(&wgpu::BufferDescriptor {
+            label: Some("Staging Buffer"),
+            size: buffer_size as u64,
+            usage: wgpu::BufferUsages::MAP_READ |   // `MAP_READ` allows it to be read (outside the shader).
+                    wgpu::BufferUsages::COPY_DST, // `COPY_DST` allows it to be the destination of the copy.
+            mapped_at_creation: false,
+        });
+
+        // Instantiates buffer with data (`numbers`).
+        let storage_buffer =
+            hardware
+                .device
+                .create_buffer_init(&wgpu::util::BufferInitDescriptor {
+                    label: Some("Storage Buffer"),
+                    contents: bytemuck::cast_slice(numbers),
+                    usage: wgpu::BufferUsages::STORAGE // `STORAGE` can be used with a bind group, and thus available to a shader.
+                        | wgpu::BufferUsages::COPY_DST // `COPY_DST` allows it to be the destination of a copy.
+                        | wgpu::BufferUsages::COPY_SRC, // `COPY_SRC` allows it to be the source of a copy.
+                });
+
+        // Load the shaders
+        let shader_modules = shader_codes
+            .iter()
+            .map(|s| {
+                hardware
+                    .device
+                    .create_shader_module(wgpu::ShaderModuleDescriptor {
+                        label: None,
+                        source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(s)),
+                    })
+            })
+            .collect::<Vec<_>>();
+
+        // Instantiates the pipeline.
+        let compute_pipelines: Vec<_> = shader_modules
+            .iter()
+            .map(|module| {
+                hardware
+                    .device
+                    .create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
+                        label: None,
+                        layout: None,
+                        module: module,
+                        entry_point: "main",
+                    })
+            })
+            .collect();
+
+        // Instantiates the bind groups, once again specifying the binding of buffers.
+        let bind_groups = compute_pipelines
+            .iter()
+            .map(|compute_pipeline| {
+                let bind_group_layout = compute_pipeline.get_bind_group_layout(0);
+                hardware
+                    .device
+                    .create_bind_group(&wgpu::BindGroupDescriptor {
+                        label: None,
+                        layout: &bind_group_layout,
+                        entries: &[wgpu::BindGroupEntry {
+                            binding: 0,
+                            resource: storage_buffer.as_entire_binding(),
+                        }],
+                    })
+            })
+            .collect();
+
+        Self {
+            hardware,
+            buffer_size,
+            staging_buffer,
+            storage_buffer,
+            compute_pipelines,
+            bind_groups,
+        }
+    }
+
+    async fn run(&self, numbers: &[u32], shader_index: usize) -> Vec<u32> {
+        // A command encoder executes one or many pipelines.
+        // It is to WebGPU what a command buffer is to Vulkan.
+        let mut encoder = self
+            .hardware
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+        {
+            let mut cpass =
+                encoder.begin_compute_pass(&wgpu::ComputePassDescriptor { label: None });
+            cpass.set_bind_group(0, &self.bind_groups[shader_index], &[]);
+            cpass.set_pipeline(&self.compute_pipelines[shader_index]);
+            cpass.dispatch_workgroups(numbers.len() as u32, 1, 1); // Number of cells to run, the (x,y,z) size of item being processed
         }
 
-        canvases.lock().unwrap().push(canvas_copy);
-    });
+        // Sets adds copy operation to command encoder.
+        // Will copy data from storage buffer on GPU to staging buffer on CPU.
+        encoder.copy_buffer_to_buffer(
+            &self.storage_buffer,
+            0,
+            &self.staging_buffer,
+            0,
+            self.buffer_size as u64,
+        );
 
-    for c in canvases.lock().unwrap().iter() {
-        canvas.combine(c);
+        // Submits command encoder for processing
+        self.hardware.queue.submit(Some(encoder.finish()));
+
+        // Sets the buffer up for mapping, sending over the result of the mapping back to us when it is finished.
+        let buffer_slice = self.staging_buffer.slice(..);
+        let (sender, receiver) = futures_intrusive::channel::shared::oneshot_channel();
+        buffer_slice.map_async(wgpu::MapMode::Read, move |v| sender.send(v).unwrap());
+
+        // Poll the device in a blocking manner so that our future resolves.
+        // In an actual application, `device.poll(...)` should
+        // be called in an event loop or on another thread.
+        self.hardware.device.poll(wgpu::Maintain::Wait);
+
+        // Awaits until `buffer_future` can be read from
+        if let Some(Ok(())) = receiver.receive().await {
+            // Gets contents of buffer
+            let data = buffer_slice.get_mapped_range();
+
+            // Since contents are received in bytes, this converts these bytes back to u32
+            let result = bytemuck::cast_slice(&data).to_vec();
+
+            // Unmaps buffer from memory, allowing it to be used again.
+            drop(data);
+            self.staging_buffer.unmap();
+
+            result
+        } else {
+            panic!("failed to run compute on gpu!")
+        }
     }
 }
