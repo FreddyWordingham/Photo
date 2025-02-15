027a052c66331abe5a9998629a4bbecb3dd75964
Freddy Wordingham <freddy@digilab.co.uk>
2025-02-15 19:13:33 +0000

    Added load colour examples

---
+++ examples/load_colour_alpha.rs
@@ -0,0 +1,11 @@
+use ndarray_images::Image;
+use palette::LinSrgba;
+
+const INPUT_DIR: &str = "input";
+const IMAGE_NAME: &str = "colour_alpha.png";
+
+fn main() {
+    let filepath = format!("{}/{}", INPUT_DIR, IMAGE_NAME);
+    let image = Image::<LinSrgba>::load(filepath).expect("Failed to load image");
+    println!("{}", image);
+}
