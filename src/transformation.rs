337aa29bc7afeecef602b8aad6efd641cd3eba05
Freddy Wordingham <freddy@digilab.co.uk>
2025-03-04 18:49:44 +0000

    Added index method to Transformation emum

--- src/transformation.rs
+++ src/transformation.rs
@@ -1,3 +1,5 @@
+use num_traits::NumCast;
+
 #[derive(Clone, Copy, PartialEq, Eq, Debug, Hash)]
 pub enum Transformation {
     Identity,
@@ -9,6 +11,24 @@ pub enum Transformation {
     FlipDiagonal,
     FlipAntiDiagonal,
 }
+
+impl Transformation {
+    pub fn index<T: NumCast>(self) -> T {
+        let i = match self {
+            Transformation::Identity => 0,
+            Transformation::Rotate90 => 1,
+            Transformation::Rotate180 => 2,
+            Transformation::Rotate270 => 3,
+            Transformation::FlipHorizontal => 4,
+            Transformation::FlipVertical => 5,
+            Transformation::FlipDiagonal => 6,
+            Transformation::FlipAntiDiagonal => 7,
+        };
+        NumCast::from(i).unwrap()
+    }
+}
+
+impl Transformation {}
 
 pub const ALL_TRANSFORMATIONS: [Transformation; 8] = [
     Transformation::Identity,
