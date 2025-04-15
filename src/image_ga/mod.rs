19885b654e35c56da841dd45a390649773a9bd04
Freddy Wordingham <freddy@digilab.co.uk>
2025-04-15 13:21:59 +0100

    Added view_tiles method to image types

--- src/image_ga/mod.rs
+++ src/image_ga/mod.rs
@@ -209,7 +209,7 @@ impl<T: Copy + PartialOrd + Zero + One> ImageGA<T> {
         ])
     }
 
-    /// Create a view of the images border.
+    /// Create a view of the image's border.
     pub fn view_border(&self, direction: Direction, size: usize) -> ArrayView3<T> {
         debug_assert!(size > 0);
         match direction {
@@ -220,7 +220,7 @@ impl<T: Copy + PartialOrd + Zero + One> ImageGA<T> {
         }
     }
 
-    /// Create a mutable view of the images border.
+    /// Create a mutable view of the image's border.
     pub fn view_border_mut(&mut self, direction: Direction, size: usize) -> ArrayViewMut3<T> {
         debug_assert!(size > 0);
         match direction {
@@ -229,6 +229,34 @@ impl<T: Copy + PartialOrd + Zero + One> ImageGA<T> {
             Direction::South => self.data.slice_mut(s![(self.height() - size).., .., ..]),
             Direction::West => self.data.slice_mut(s![.., 0..size, ..]),
         }
+    }
+
+    /// Create a view of tiles in the image.
+    pub fn view_tiles(&self, tile_size: usize, overlap: usize) -> Array2<ArrayView3<T>> {
+        let (height, width) = (self.height(), self.width());
+        debug_assert!(tile_size < overlap);
+        debug_assert!(height >= tile_size);
+        debug_assert!(width >= tile_size);
+        debug_assert_eq!(
+            (width - overlap) % (tile_size - overlap),
+            0,
+            "Image must contain an integer number of tiles"
+        );
+        debug_assert_eq!(
+            (height - overlap) % (tile_size - overlap),
+            0,
+            "Image must contain an integer number of tiles"
+        );
+
+        let num_horizontal_tiles = (width - overlap) / (tile_size - overlap);
+        let num_vertical_tiles = (height - overlap) / (tile_size - overlap);
+
+        let step_size = tile_size - overlap;
+        Array2::from_shape_fn((num_vertical_tiles, num_horizontal_tiles), |(y, x)| {
+            let start_y = y * step_size;
+            let start_x = x * step_size;
+            self.view([start_y, start_x], [tile_size, tile_size])
+        })
     }
 }
 
